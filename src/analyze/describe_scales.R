b5d21686f3b41b50fcc57d6703592ae06afd31dd
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2020-12-21 18:13:52 -0700

    Calculate descriptive statistics of measures, correlation table

--- src/analyze/describe_scales.R
+++ src/analyze/describe_scales.R
@@ -1,6 +1,8 @@
 # Dependencies
+library(Hmisc)
 library(tidyverse)
 library(RBtest)
+library(psych)
 
 # Import
 redcap <- read_csv("data/raw/redcap_cleaned.csv")
@@ -49,6 +51,144 @@ redcap_scales %>%
   filter(is.na(score)) %>%
   nrow()
 
+# INTERNAL CONSISTENCY ----------------------------------------------------
+
+# Interpersonal Sexual Objectification Scale 
+redcap_scales %>% 
+  select(starts_with("isos")) %>%
+  alpha()
+
+# Gender Identity Microaggressions Scale
+redcap_scales %>%
+  select(starts_with("gims")) %>%
+  alpha()
+
+# Self-Objectification Beliefs and Behaviors Scale 
+redcap_scales %>%
+  select(starts_with("sobbs")) %>%
+  alpha()
+
+# Transgender Identity Survey 
+redcap_scales %>%
+  select(starts_with("tis")) %>%
+  alpha()
+
+# Personal Feelings Questionnaire-2 
+redcap_scales %>%
+  select(starts_with("pfq2s")) %>%
+  alpha()
+
+# Mental Health Inventory 
+redcap_scales %>%
+  select(starts_with("mhi")) %>%
+  alpha()
+
+# TOTAL SCORES & DESCRIPTIVES ----------------------------------------------
+
+# ISOS - mean
+isos <- redcap_scales %>%
+  select(record_id, starts_with("isos")) %>%
+  gather(key = "measure", value = "score", -record_id) %>%
+  group_by(record_id) %>%
+  summarize(
+    total_isos = mean(score)
+  ) %>%
+  select(-record_id)
+isos
+
+# TIS - mean
+tis <- redcap_scales %>%
+  select(record_id, starts_with("tis")) %>%
+  gather(key = "measure", value = "score", -record_id) %>%
+  group_by(record_id) %>%
+  summarize(
+    total_tis = mean(score)
+  ) %>%
+  select(-record_id)
+tis
+
+# SOBBS - mean
+sobbs <- redcap_scales %>%
+  select(record_id, starts_with("sobbs")) %>%
+  gather(key = "measure", value = "score", -record_id) %>%
+  group_by(record_id) %>%
+  summarize(
+    total_sobbs = mean(score)
+  ) %>%
+  select(-record_id)
+sobbs
+
+# GIMS - sum
+gims <- redcap_scales %>%
+  select(record_id, starts_with("gims")) %>%
+  gather(key = "measure", value = "score", -record_id) %>%
+  group_by(record_id) %>%
+  summarize(
+    total_gims = sum(score)
+  ) %>%
+  select(-record_id)
+gims  
+
+# PFQ2S - sum
+pfq2s <- redcap_scales %>%
+  select(record_id, starts_with("pfq2s")) %>%
+  gather(key = "measure", value = "score", -record_id) %>%
+  group_by(record_id) %>%
+  summarize(
+    total_pfq2s = sum(score)
+  ) %>%
+  select(-record_id)
+pfq2s
+
+# MHI - sum
+mhi <- redcap_scales %>%
+  select(record_id, starts_with("mhi")) %>%
+  gather(key = "measure", value = "score", -record_id) %>%
+  group_by(record_id) %>%
+  summarize(
+    total_mhi = sum(score)
+  ) %>%
+  select(-record_id)
+mhi  
+
+# Bind columns
+scales_total <- redcap_scales %>%
+  bind_cols(isos, tis, sobbs, gims, pfq2s, mhi)
+scales_total
+
+# Mean, SD, range
+scales_total %>%
+  select(starts_with("total")) %>%
+  gather(key = "measure", value = "score") %>%
+  group_by(measure) %>%
+  summarise(
+    M = mean(score),
+    SD = sd(score),
+    MIN = min(score),
+    MAX = max(score)
+  )
+
+# Prepare for the correlation matrix
+totals_only <- scales_total %>%
+  select(starts_with("total"))
+
+# Bivariate correlations
+rcorr(as.matrix(totals_only), type = "pearson")
+
+# Save correlation matrix
+cor_list <- rcorr(as.matrix(totals_only), type = "pearson") %>%
+  as.list()
+
+# Convert to table
+cor_table <- cor_list$r %>%
+  as.data.frame() %>%
+  rownames_to_column() %>%
+  as_tibble()
+
 # EXPORT SCALES -----------------------------------------------------------
 
-# Write to file
+# Export correlation table
+write_csv(cor_table, path = "data/results/cor_table.csv")
+
+# Export total scales
+write_csv(scales_total, path = "data/results/redcap_scales.csv")
