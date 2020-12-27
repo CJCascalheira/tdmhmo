3488160a54206a8711b5a18e259f87a59220aae9
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2020-12-27 12:40:05 -0600

    CFA goodness of fit and LV intercorrelations

--- src/analyze/sem_measurement_model.R
+++ src/analyze/sem_measurement_model.R
@@ -16,7 +16,7 @@ redcap_scales
 # CONFIRMATORY FACTOR ANALYSIS --------------------------------------------
 
 # Vector of standard deviations
-redcap_sd_vector <- redcap_scales %>%
+tdmhmo_sd_vector <- redcap_scales %>%
   gather(key = "measure", value = "score") %>%
   group_by(measure) %>%
   summarize(
@@ -24,18 +24,18 @@ redcap_sd_vector <- redcap_scales %>%
   ) %>%
   select(sd) %>%
   pull()
-redcap_sd_vector
+tdmhmo_sd_vector
 
 # Correlation matrix
-redcap_cor_matrix <- cor(redcap_scales)
-redcap_cor_matrix
+tdmhmo_cor_matrix <- cor(redcap_scales)
+tdmhmo_cor_matrix
 
 # Create covariance matrix
-redcap_cov <- cor2cov(redcap_cor_matrix, redcap_sd_vector)
-redcap_cov
+tdmhmo_cov <- cor2cov(tdmhmo_cor_matrix, tdmhmo_sd_vector)
+tdmhmo_cov
 
 # Latent variable model
-redcap_model <- '
+tdmhmo_model <- '
   gims =~ a*gims_factor_1 + b*gims_factor_2 + c*gims_factor_3 + d*gims_factor_4 + e*gims_factor_5
   isos =~ f*isos_factor_1 + g*isos_factor_2 + h*isos_factor_3
   sobbs =~ i*sobbs_factor_1 + j*sobbs_factor_2
@@ -45,11 +45,59 @@ redcap_model <- '
 '
 
 # Fit the data to the model using CFA
-redcap_fit <- cfa(
-  model = redcap_model,
-  sample.cov = redcap_cov,
+tdmhmo_fit <- cfa(
+  model = tdmhmo_model,
+  # Complete data necessary
+  data = redcap_scales,
+  sample.cov = tdmhmo_cov,
   sample.nobs = 583,
-  std.lv = FALSE
+  std.lv = FALSE,
+  # Maximum likelihood estimation with robust standard errors
+  estimator = "MLR"
 )
- # Summarize the model
-summary(redcap_fit, standardized = TRUE)
+
+# Summarize the model
+summary(tdmhmo_fit, standardized = TRUE, fit.measures = TRUE)
+
+# SIGNIFICANT INTERCORRELATIONS AMONG LATENT VARIABLES --------------------
+
+# Correlations among the latent variables (LVs)
+lv_cor_matrix <- lavInspect(tdmhmo_fit, what = "cor.lv")
+lv_cor_matrix
+
+# Matrix to data fram
+lv_cor_df <- lv_cor_matrix %>%
+  as.data.frame() %>%
+  as_tibble()
+lv_cor_df
+
+# Calculate t-value cutoff for two-tailed significant correlation 
+t_cutoff <- qt(0.05/2, df = 581) %>%
+  # Take the absolute value
+  abs()
+t_cutoff 
+
+# Function to calculate critical t-value
+t_stat_cor <- function(r) {
+  (r * sqrt(583 - 2)) / (sqrt(1 - r^2))
+}
+
+# Calculate t-values for all correlations among LVs
+lv_t_values <- map_df(lv_cor_df, t_stat_cor)
+lv_t_values
+
+# Calculate p-values manually
+# GIMS
+pt(lv_t_values$gims[2], df = 581, lower.tail = FALSE)
+pt(lv_t_values$gims[3], df = 581, lower.tail = FALSE)
+pt(lv_t_values$gims[4], df = 581, lower.tail = FALSE)
+pt(lv_t_values$gims[5], df = 581, lower.tail = FALSE)
+pt(lv_t_values$gims[6], df = 581, lower.tail = TRUE)
+# ISOS
+pt(lv_t_values$isos[3], df = 581, lower.tail = FALSE)
+pt(lv_t_values$isos[4], df = 581, lower.tail = FALSE)
+pt(lv_t_values$isos[5], df = 581, lower.tail = FALSE)
+pt(lv_t_values$isos[6], df = 581, lower.tail = TRUE)
+
+# Therefore, since all values are significant at the p < .001 thus far and t = 2.2 is the lowest
+# t-value, all remaining intercorrelations among LVs are significant at the p < .001 level.
