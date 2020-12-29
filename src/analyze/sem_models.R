d4592f5f2e988f123dbdf408d0d1ef8decd9f00e
Cory Cascalheira <17078341+corycaaz@users.noreply.github.com>
2020-12-29 12:02:44 -0600

    Specified SEM correctly with proper syntax

--- src/analyze/sem_models.R
+++ src/analyze/sem_models.R
@@ -177,8 +177,6 @@ tdmhmo_sem <- '
   mhi_s := SM * DS
   mhi_so := SOM * DSO
   mhi_it := ITM * DIT
-  shame_so := SOS * DSO
-  shame_it := ITS * DIT
   
 # Total effect
   total := DM + (SM * DS) + (SOM * DSO) + (ITM * DIT)
@@ -240,8 +238,6 @@ tdmhmo_sem_2 <- '
   mhi_s := SM * DS
   mhi_so := SOM * DSO
   mhi_it := ITM * DIT
-  shame_so := SOS * DSO
-  shame_it := ITS * DIT
   
 # Total effect
   total := DM + (SM * DS) + (SOM * DSO) + (ITM * DIT)
@@ -283,7 +279,8 @@ lavInspect(tdmhmo_sem_fit_2, what = "cor.lv")
 
 # STRUCTURAL MODEL 3 ------------------------------------------------------
 
-# Remove the second-order dehumanization factor
+# Remove the second-order dehumanization factor and covary theoretically
+# similar internalization constructs (see lab notebook).
 # Comment out indirect effect to diagnose the Heywood case
 tdmhmo_sem_3 <- '
 # Define the measurement model
@@ -295,7 +292,11 @@ tdmhmo_sem_3 <- '
   tis =~ tis_factor_1 + tis_factor_2 + tis_factor_3 + tis_factor_4
   pfq2s =~ pfq2s_parcel_1 + pfq2s_parcel_2 + pfq2s_parcel_3
   mhi =~ mhi_parcel_1 + mhi_parcel_2
-  
+
+# Covariance among LVs
+  tis ~~ sobbs
+  gims ~~ isos 
+
 # Define structural relationships
 
 # Direct effects
@@ -305,14 +306,13 @@ tdmhmo_sem_3 <- '
   tis ~ GIT*gims
   
 # Indirect effects
-#  mhi_s := SM * DS
-#  mhi_so := SOM * DSO
-#  mhi_it := ITM * DIT
-#  shame_so := SOS * DSO
-#  shame_it := ITS * DIT
+  mhi_s_is := ISS * SM
+  mhi_s_g := GS * SM 
+  mhi_so := ISSO * SOM
+  mhi_it := GIT * ITM
   
 # Total effect
-#  total := DM + (SM * DS) + (SOM * DSO) + (ITM * DIT)
+  total := GM + ISM + (GS * SM) + (SM * GS) + (ISSO * SOM) + (GIT * ITM)
 '
 
 # Fit the model to the data - sem
@@ -335,25 +335,73 @@ semPaths(
   layout = "tree2"
 )
 
-##################################################################################
-##################################################################################
-##################################################################################
+# STRUCTURAL MODEL 4 ------------------------------------------------------
 
-# Fit the model to the data - lavaan
-tdmhmo_sem_fit_3 <- lavaan(
-  model = tdmhmo_sem_3, 
+# Specify the SEM, returning to second-order factor
+tdmhmo_sem_4 <- '
+# Define the measurement model
+
+# First-order factors
+  gims =~ gims_factor_1 + gims_factor_2 + gims_factor_3 + gims_factor_4 + gims_factor_5
+  isos =~ isos_factor_1 + isos_factor_2 + isos_factor_3
+  sobbs =~ sobbs_factor_1 + sobbs_factor_2
+  tis =~ tis_factor_1 + tis_factor_2 + tis_factor_3 + tis_factor_4
+  pfq2s =~ pfq2s_parcel_1 + pfq2s_parcel_2 + pfq2s_parcel_3
+  mhi =~ mhi_parcel_1 + mhi_parcel_2
+  
+# Second-order factor
+  dehum =~ gims + isos
+
+# Covariance among LVs
+  tis ~~ sobbs
+
+# Define structural relationships
+
+# Direct effects
+  mhi ~ DM*dehum + SM*pfq2s + SOM*sobbs + ITM*tis
+  pfq2s ~ DS*dehum + SOS*sobbs + ITS*tis
+  sobbs ~ DSO*dehum
+  tis ~ DIT*dehum
+  
+# Indirect effects
+  mhi_s := DS * SM
+  mhi_so := DSO * SOM
+  mhi_it := DIT * ITM
+  
+# Total effect
+  total := DM + (DS * SM) + (DSO * SOM) + (DIT * ITM)
+'
+
+# Fit the model to the data
+tdmhmo_sem_fit_4 <- sem(
+  model = tdmhmo_sem_4, 
   data = redcap_scales,
   # Maximum likelihood estimation with robust standard errors
-  estimator = "MLM",
-  # Mimic the call to sem()
-  int.ov.free = TRUE, 
-  int.lv.free = FALSE, 
-  auto.fix.first = TRUE, 
-  auto.fix.single = TRUE, 
-  auto.var = TRUE, 
-  auto.cov.lv.x = FALSE, 
-  auto.efa = TRUE,
-  auto.th = TRUE, 
-  auto.delta = TRUE, 
-  auto.cov.y = FALSE
+  estimator = "MLM"
 )
+
+# Summarize the model
+summary(tdmhmo_sem_fit_4, standardized = TRUE, fit.measures = TRUE)
+
+# Correlations among the LVs
+lavInspect(tdmhmo_sem_fit_4, what = "cor.lv")
+
+# Visualize the SEM
+semPaths(
+  object = tdmhmo_sem_fit_4,
+  layout = "tree2"
+)
+
+###########################################################################
+
+# Structural Model 1 - Heywood Case
+tdmhmo_sem_fit
+
+# Structural Model 2 - No Heywood Case
+tdmhmo_sem_fit_2
+
+# Structural Model 3 - No Dehumanization Factor
+tdmhmo_sem_fit_3
+
+# Structural Model 4 - Dehumanization Factor Retained
+tdmhmo_sem_fit_4
